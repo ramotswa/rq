ae21ab8e2ef3d8cb874e74ff9b4627af6adbcc9d
ramotswa <toby@Tobys-MacBook-Air.local>
2013-10-28 21:22:47 +0000

    First deploy

---
+++ dbConnector.php
@@ -0,0 +1,10 @@
+﻿<?php
+// Create connection
+$con=mysqli_connect("ramotswa.com","ramotswa","setunia1","roadQuality");
+
+// Check connection
+if (mysqli_connect_errno($con))
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+?>
